0901323484eba4a2a6f93b02e68d045a4fb6085e
omkarjagtap2212 <omkarjagtap2312@gmail.com>
2024-06-14 02:09:23 +0530

    added to github

---
+++ src/constant/index.js
@@ -0,0 +1,117 @@
+// import project1 from "../assets/projects/project-1.png";
+// // import project2 from "../assets/projects/project-2.png";
+// // import project3 from "../assets/projects/project-3.png";
+// // import project4 from "../assets/projects/project-4.png";
+
+// export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With one year of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies such as Node.js, Express.js, MongoDB, and NGINX. I also have practical experience with DevOps tools including Docker, AWS S3, ECS, ECR, EC2, Lambda, and Elastic Beanstalk. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`
+// export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including JavaScript, React, Next.js, TypeScript, Node.js,Express.js and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy travelling, reading books, and exploring new technologies.`;
+// // export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+// export const EXPERIENCES = [
+//   {
+//     year: "DEC 2022 - MAR 2023",
+//     role: "Full Stack Developer",
+//     company: "NULLCLASS India ",
+//     description: `I and my team are developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with PostgreSQL databases. Collaborated with stakeholders to define project requirements and timelines.`,
+//     technologies: ["Typescript", "React.js", "Node.js", "Express.js"],
+//   },
+//   {
+//     year: " MAY 2023 - current",
+//     role: "Full Stack Developer",
+//     company: "Upwork- Freelancer ",
+//     description: `I and my team are developing and maintaining web applications using Typescript, React.js, and Node.js. Implemented RESTful APIs and integrated with PostgreSQL databases. Collaborated with stakeholders to define project requirements and timelines.`,
+//     technologies: ["Typescript", "React.js", "Node.js", "Express.js"],
+//   },
+
+
+
+// ];
+
+// export const PROJECTS = [
+//   {
+//     title: "Ecommerce webisite -for upwork client",
+//     image: project1,
+//     link: "",
+//     code: "",
+//     description:
+//       "A platform developed to eliminate the repetitive manual task of calculating ranks for juniors in every contest.  It fetches contestants' performance data, assigns scores, and maintains a live scoreboard. Admins can manage contests and contestants with ease. It widely used by college students.",
+//     technologies: ["Typescript", "React", "Node.js", "MongoDB","Express.js"],
+//   },
+
+
+
+// ];
+
+// export const CONTACT = {
+//   address: "Pune, ,Maharashtra, India ",
+//   phoneNo: "+91 8788349193 ",
+//   email: "omkarjagtap1947@gmail.com",
+// };
+
+
+
+import project1 from "../assets/projects/project-1.png";
+// import project2 from "../assets/projects/project-2.png";
+// import project3 from "../assets/projects/project-3.png";
+// import project4 from "../assets/projects/project-4.png";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With one year of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies such as Node.js, Express.js, MongoDB, and NGINX. I also have practical experience with DevOps tools including Docker, AWS S3, ECS, ECR, EC2, Lambda, and Elastic Beanstalk. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including JavaScript, React, Next.js, TypeScript, Node.js, Express.js, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy traveling, reading books, and exploring new technologies.`;
+
+export const EXPERIENCES = [
+  {
+    year: "DEC 2022 – MAY 2023",
+    role: "MERN Stack Training",
+    company: "NULLCLASS Remote – Pune, Maharashtra, India",
+    description: `Designed and developed dynamic and responsive websites using HTML, CSS, JavaScript, and ReactJS. Backend development with Node.js and Express.js. Worked with the Mongoose ODM library for MongoDB database integration and MongoDB aggregation for advanced data processing and analysis. Implemented user authentication and authorization features using JWT. Learned various technologies including State Management with Redux, Deployment and DevOps (basic), Docker, Version Control with Git, and Testing APIs using Postman.`,
+    technologies: ["HTML", "CSS", "JavaScript", "ReactJS", "Node.js", "Express.js", "MongoDB", "JWT", "Redux", "Docker", "Git", "Postman"],
+  },
+
+  {
+    year: "Jun 2023 - current",
+    role: "Freelance Web Developer (Sosua Villas Project)",
+    company: "Upwork - Client: Based in Puerto Rico",
+    description: "Developed and designed the Sosua Villas website, ensuring a user-friendly interface and an engaging user experience for luxury villa rentals. Created a responsive website layout compatible with various devices to reach a broader audience. Implemented SEO best practices to improve the website’s search engine ranking and increase organic traffic. Optimized website performance to ensure fast loading times and smooth navigation. Integrated third-party services such as Google Maps for location services and a payment gateway for secure online transactions. Conducted training sessions for the client to efficiently use and manage the website",
+    technologies: ["Typescript","Node.js","Express.js","ReactJS", "SEO", "Google Maps API", "Payment Gateway Integration"],
+  },
+
+];
+
+export const PROJECTS = [
+  {
+    title: "Ecommerce website - for Upwork client",
+    image: project1,
+    link: "https://test-frontend-teal.vercel.app/",
+    code: "https://github.com/omkarjagtap2212/test-frontend",
+    description:
+      "A platform developed to eliminate the repetitive manual task of calculating ranks for juniors in every contest. It fetches contestants' performance data, assigns scores, and maintains a live scoreboard. Admins can manage contests and contestants with ease. It is widely used by college students.",
+    technologies: ["Typescript", "React", "Node.js", "MongoDB", "Express.js"],
+  },
+
+  {
+    title: "Front-end: Blog Website- WORKING ON THIS SITE",
+    image: project1, // Placeholder image, replace with the actual image if available
+    link: "https://github.com/yourusername/blog-website", // Replace with the actual link
+    code: "https://github.com/yourusername/blog-website", // Replace with the actual code link
+    description:
+      "Designed and developed a clean and modern blog website using HTML, CSS, and JavaScript. Optimized website for speed and user experience using server-side rendering. Utilized Appwrite (BaaS) as backend as a service and deployed on GitHub Pages via GitHub Actions.",
+    technologies: ["React.js", "Redux Toolkit", "Appwrite", "Tailwind CSS"],
+  },
+
+  {
+    title: "Backend: Streaming Platform",
+    image: project1, // Placeholder image, replace with the actual image if available
+    link: "https://github.com/yourusername/streaming-platform", // Replace with the actual link
+    code: "https://github.com/omkarjagtap2212/streaming-app-backend", // Replace with the actual code link
+    description:
+      "Designed and developed a backend application using Node.js, Express.js, Socket.io, and Docker to replicate a streaming platform. Utilized FFmpeg within Docker containers for CPU-intensive video processing tasks, enabling efficient video transcoding and format conversion for seamless streaming. Addressed challenges related to CPU limitations during video processing by leveraging Docker containers for isolated and efficient execution of FFmpeg tasks. Maintained a robust connection between video data and FFmpeg for seamless integration and encryption of video files, enhancing data security and integrity. This backend application facilitates live streaming capabilities, enabling content creators to capture, process, and broadcast video content to viewers in real-time.",
+    technologies: ["Node.js", "Express.js", "Docker", "FFmpeg", "Socket.io", "Git"],
+  },
+];
+
+export const CONTACT = {
+  address: "Pune, Maharashtra, India",
+  phoneNo: "+91 8788349193",
+  email: "omkarjagtap1947@gmail.com",
+};
