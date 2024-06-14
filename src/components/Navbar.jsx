79e6eb9b5d258c252c5d0907c0e59188e86b4fda
omkarjagtap2212 <omkarjagtap2312@gmail.com>
2024-06-14 12:00:40 +0530

    added some chnages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,19 +15,17 @@ const NavBar = () => {
             {/* <?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools --> */}
 <svg fill="#aaaaaa" width="45px" height="45px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M1.293,11.293l4-4A1,1,0,1,1,6.707,8.707L3.414,12l3.293,3.293a1,1,0,1,1-1.414,1.414l-4-4A1,1,0,0,1,1.293,11.293Zm17.414-4a1,1,0,1,0-1.414,1.414L20.586,12l-3.293,3.293a1,1,0,1,0,1.414,1.414l4-4a1,1,0,0,0,0-1.414ZM13.039,4.726l-4,14a1,1,0,0,0,.686,1.236A1.053,1.053,0,0,0,10,20a1,1,0,0,0,.961-.726l4-14a1,1,0,1,0-1.922-.548Z"/></svg>
         </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-          <a href="https://www.linkedin.com/in/pratiksonthaliya" target="_blank" rel="noopener noreferrer">
+        <div className="m-8 flex items-center justify-center gap-10 text-2xl">
+          <a href="https://www.linkedin.com/in/omkarjagtap1947" target="_blank" rel="noopener noreferrer">
             <FaLinkedin/>
           </a>
-          <a href="https://github.com/pratiksonthaliya" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/omkarjagtap2212" target="_blank" rel="noopener noreferrer">
             <FaGithub/>
           </a>
-          <a href="https://twitter.com/SonthaliyaP" target="_blank" rel="noopener noreferrer">
+          <a href="https://x.com/masterorj" target="_blank" rel="noopener noreferrer">
             <FaSquareXTwitter/>
           </a>
-          <a href="https://www.instagram.com/pratiksonthaliya" target="_blank" rel="noopener noreferrer">
-            <FaInstagram/>
-          </a>
+         
         </div>
     </nav>
   )
--- src/constant/index.js
+++ src/constant/index.js
@@ -50,7 +50,10 @@
 
 
 
-import project1 from "../assets/projects/project-1.png";
+import project1 from "../assets/projects/project05.png";
+import project2 from "../assets/projects/project06.png";
+
+
 // import project2 from "../assets/projects/project-2.png";
 // import project3 from "../assets/projects/project-3.png";
 // import project4 from "../assets/projects/project-4.png";
@@ -69,11 +72,11 @@ export const EXPERIENCES = [
   },
 
   {
-    year: "Jun 2023 - current",
+    year: "JUN 2023 - CURRENT",
     role: "Freelance Web Developer (Sosua Villas Project)",
     company: "Upwork - Client: Based in Puerto Rico",
     description: "Developed and designed the Sosua Villas website, ensuring a user-friendly interface and an engaging user experience for luxury villa rentals. Created a responsive website layout compatible with various devices to reach a broader audience. Implemented SEO best practices to improve the website’s search engine ranking and increase organic traffic. Optimized website performance to ensure fast loading times and smooth navigation. Integrated third-party services such as Google Maps for location services and a payment gateway for secure online transactions. Conducted training sessions for the client to efficiently use and manage the website",
-    technologies: ["Typescript","Node.js","Express.js","ReactJS", "SEO", "Google Maps API", "Payment Gateway Integration"],
+    technologies: ["Typescript", "Node.js", "ReactJS", "SEO", "Google Maps API", "Payment Gateway Integration"],
   },
 
 ];
@@ -85,24 +88,23 @@ export const PROJECTS = [
     link: "https://test-frontend-teal.vercel.app/",
     code: "https://github.com/omkarjagtap2212/test-frontend",
     description:
-      "A platform developed to eliminate the repetitive manual task of calculating ranks for juniors in every contest. It fetches contestants' performance data, assigns scores, and maintains a live scoreboard. Admins can manage contests and contestants with ease. It is widely used by college students.",
+      "I built an ecommerce website for selling items similar to Amazon, Flipkart, and Myntra. The platform includes features like a hybrid login system using Firebase and MongoDB for extra security. It also has a product dashboard where admin users can add or remove products and manage shipping features dynamically. Additionally, the dashboard provides real-time stock monitoring.",
     technologies: ["Typescript", "React", "Node.js", "MongoDB", "Express.js"],
   },
-
+  
   {
-    title: "Front-end: Blog Website- WORKING ON THIS SITE",
+    title: "Front-end: Blog Website - WORKING ON THIS SITE",
     image: project1, // Placeholder image, replace with the actual image if available
-    link: "https://github.com/yourusername/blog-website", // Replace with the actual link
-    code: "https://github.com/yourusername/blog-website", // Replace with the actual code link
+    link: "https://github.com/omkarjagtap2212/appwriteblog", // Replace with the actual link
+    code: "https://github.com/omkarjagtap2212/appwriteblog", // Replace with the actual code link
     description:
       "Designed and developed a clean and modern blog website using HTML, CSS, and JavaScript. Optimized website for speed and user experience using server-side rendering. Utilized Appwrite (BaaS) as backend as a service and deployed on GitHub Pages via GitHub Actions.",
     technologies: ["React.js", "Redux Toolkit", "Appwrite", "Tailwind CSS"],
   },
-
   {
     title: "Backend: Streaming Platform",
-    image: project1, // Placeholder image, replace with the actual image if available
-    link: "https://github.com/yourusername/streaming-platform", // Replace with the actual link
+    image: project2, // Placeholder image, replace with the actual image if available
+    link: "https://screenrec.com/share/Jk38pme0ZP", // Replace with the actual link
     code: "https://github.com/omkarjagtap2212/streaming-app-backend", // Replace with the actual code link
     description:
       "Designed and developed a backend application using Node.js, Express.js, Socket.io, and Docker to replicate a streaming platform. Utilized FFmpeg within Docker containers for CPU-intensive video processing tasks, enabling efficient video transcoding and format conversion for seamless streaming. Addressed challenges related to CPU limitations during video processing by leveraging Docker containers for isolated and efficient execution of FFmpeg tasks. Maintained a robust connection between video data and FFmpeg for seamless integration and encryption of video files, enhancing data security and integrity. This backend application facilitates live streaming capabilities, enabling content creators to capture, process, and broadcast video content to viewers in real-time.",
